1009330280e5ece40cbc48b0addbddc13e182010
Pascual Felicio <pascualfelicio@gmail.com>
2025-01-22 13:10:43 +0100

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const ul = document.querySelector('ul');
 const btnAddBook = document.querySelector('.addBook');
 const table = document.querySelector('table');
+const btnSubmit = document.querySelector('.btnSubmit');
+const bookName = document.querySelector(".book-name");
+const bookAuthor = document.querySelector(".book-author");
+const bookPages = document.querySelector(".book-pages");
+const modal = document.querySelector(".container-modal");
+const closeModal = document.querySelector(".closeModal");
+
+const read = false;
+let countBooks = 0
 
 const myLibrary = [
-    {
+    /* {
         title: 'The Hobbit',
         author: 'J.R.R Tolkien',
         pages: 320
@@ -22,39 +31,55 @@ const myLibrary = [
         title: 'Stormlight Archive',
         author: 'Brandon Sanderson',
         pages: 1001
-    }
+    } */
 
 ];
 
-const Book = () => {
-
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.newBook = addBookToLibrary(this.title, this.author, this.pages);
 }
 
-const addBookToLibrary = () => {
-
+const addBookToLibrary = (title, author, pages) => {
+    const newBook = {
+        title: title,
+        author: author,
+        pages: pages
+    }
+    return newBook;
 }
 
-myLibrary.forEach((book) => {
-    const tr = document.createElement("tr");
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
-    const td3 = document.createElement("td");
-    const td4 = document.createElement("td");
-    var btnEl = document.createElement("button")
-    td1.textContent = book.title;
-    td2.textContent = book.author;
-    td3.textContent = book.pages;
-    btnEl.textContent = `X`;
-    btnEl.classList.add('deleteBtn')
-    table.appendChild(tr)
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    td4.appendChild(btnEl);
-    
-   
-})
+const updateLibrary = () => {
+    myLibrary.forEach((book) => {
+        console.log(book)
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const td4 = document.createElement("td");
+        const td5 = document.createElement("td");
+        var btnEl = document.createElement("button");
+        var btnRead = document.createElement("button");
+        td1.textContent = book.title;
+        td2.textContent = book.author;
+        td3.textContent = book.pages;
+        td5.textContent = 'Not read yet';
+        btnEl.textContent = `X`;
+        btnEl.classList.add('deleteBtn');
+        btnRead.textContent = `Read it?`;
+        btnRead.classList.add('readBtn');
+        table.appendChild(tr)
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        td4.appendChild(btnEl);
+        td5.appendChild(btnRead);
+    })
+}
 
 const rowDelete = (e) => {
     if (!e.target.classList.contains('deleteBtn')) {
@@ -65,8 +90,31 @@ const rowDelete = (e) => {
     btn.closest('tr').remove()
 }
 
-table.addEventListener('click', rowDelete)
+table.addEventListener('click', rowDelete);
 
 btnAddBook.addEventListener('click', (e) => {
-    e.preventDefault()
+    modal.style.display = 'flex';
 })
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const addingBook = new Book(bookName.value, bookAuthor.value, bookPages.value);
+    bookName.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+    newBookAdd = addingBook.newBook
+    //console.log(newBookAdd)
+    
+    //console.log(myLibrary.length)
+    myLibrary.push(addingBook.newBook)
+    console.log(myLibrary[0].title)
+    table.remove()
+    updateLibrary()
+    modal.style.display = 'none';
+});
+
+closeModal.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+})
+
+
